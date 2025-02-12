c3c61175ac7b9a8e840317ceab51d8e30a1ec989
kishanchauhan01 <kishanchauhan2006.25@gmail.com>
2025-02-12 19:46:40 +0530

    fix setting page

--- client/pages/settings/index.php
+++ client/pages/settings/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Settings</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <?php include '../../public/css/bootstap.html' ?>
+    <?php include '../../../public/css/bootstap.html' ?>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <style>
