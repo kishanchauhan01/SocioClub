e91213d4a7ad882b62077e7d88b6ef3431bb5b1c
AnshD07 <dholakiyaansh1707@gmail.com>
2025-02-25 21:58:21 +0530

    reamaning title added

--- client/pages/Guest/pages/navbar.php
+++ client/pages/Guest/pages/navbar.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>E-Commerce Home</title>
+  <title>SocioClub Home</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
